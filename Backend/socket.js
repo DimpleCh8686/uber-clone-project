2c3864c864a0b1b6eb683599b97c25a4115b9cf9
DimpleCh8686 <choudharydimple1406@gmail.com>
2025-07-11 19:21:52 +0530

    hosted

--- Backend/socket.js
+++ Backend/socket.js
@@ -7,8 +7,9 @@ let io;
 function initializeSocket(server){
     io = socketIo(server, {
         cors: {
-            origin: '*',
-            methods: ['GET', 'POST']
+            origin: ['http://localhost:5173', 'https://uber-clone-project-seven.vercel.app'],
+            methods: ['GET', 'POST'],
+            credentials: true
         }
     });
 
